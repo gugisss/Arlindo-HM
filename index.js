003e7e3b014ffe57b09687f7507f20b5a0fad19f
gugisss <gugufm2015@gmail.com>
2024-01-15 19:41:47 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+       e.preventDefault();
+
+       document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+       });
+    });
+ });
